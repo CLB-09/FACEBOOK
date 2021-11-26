b70c7cc54717957b85f78906f61d4d14eb212983
Ç.L.ß <55603039+CLB-09@users.noreply.github.com>
2021-11-26 18:43:16 +0500

    Update FB-HACK.py

--- FB-HACK.py
+++ FB-HACK.py
@@ -1,4 +1,4 @@
-#Note: CLB-09 !
+#Note: CLB-09 !!
 import os, sys, time, datetime, random, hashlib, re, threading, json, getpass, urllib, requests, mechanize
 from multiprocessing.pool import ThreadPool
 
